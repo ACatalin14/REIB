461e08422f1e99982130d96108b03dc3eab0d4f7
ACatalin14 <catalinanastas@gmail.com>
2022-11-28 23:05:57 +0200

    Now using lighter dependency chrome-aws-lambda

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import { consoleLog } from './Helpers/Utils.js';
+import Chromium from 'chrome-aws-lambda';
 
 consoleLog('REIB has been deployed!');
+
+(async () => {
+    const path = await Chromium.executablePath;
+    consoleLog(`Chromium can be found at ${path}`);
+})();
