f69fd426c7133a044f26743b41ef36df4796eebc
ACatalin14 <catalinanastas@gmail.com>
2022-11-29 22:49:28 +0200

    Checking /tmp path

--- index.js
+++ index.js
@@ -8,19 +8,11 @@ consoleLog('REIB has been deployed!');
 (async () => {
     consoleLog('Ready to download Chromium.');
 
-    if (fs.existsSync('/tmp/.local-chromium')) {
-        consoleLog('The path /tmp/.local-chromium exists!');
+    if (fs.existsSync('/tmp')) {
+        consoleLog('The path /tmp exists!');
     } else {
-        consoleLog('The path /tmp/.local-chromium does not exist! Creating it.');
-        fs.mkdirSync('/tmp/.local-chromium', { recursive: true });
-    }
-
-    const cachePath = `${os.homedir()}/.chromium-cache`;
-    if (fs.existsSync(cachePath)) {
-        consoleLog(`The path ${cachePath} exists!`);
-    } else {
-        consoleLog(`The path ${cachePath} does not exist! Creating it.`);
-        fs.mkdirSync(cachePath, { recursive: true });
+        consoleLog('The path /tmp does not exist! Creating it.');
+        fs.mkdirSync('/tmp', { recursive: true });
     }
 
     const path = await downloadChromium({
