5314e3961e277fb2c5ee82874a6b7d8dedf73997
ACatalin14 <catalinanastas@gmail.com>
2022-11-29 22:27:15 +0200

    Added creation of /tmp/.local-chromium folder

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import { consoleLog } from './Helpers/Utils.js';
 import downloadChromium from 'download-chromium';
+import fs from 'fs';
 
 consoleLog('REIB has been deployed!');
 
 (async () => {
     consoleLog('Ready to download Chromium.');
-    const path = await downloadChromium();
+
+    if (fs.existsSync('/tmp/.local-chromium')) {
+        consoleLog('The path /tmp/.local-chromium exists!');
+    } else {
+        consoleLog('The path /tmp/.local-chromium does not exist! Creating it.');
+        fs.mkdirSync('/tmp/.local-chromium', { recursive: true });
+    }
+
+    const path = await downloadChromium({
+        installPath: '/tmp/.local-chromium',
+        log: true,
+    });
+
     consoleLog(`Downloaded Chromium to ${path}`);
 })();
